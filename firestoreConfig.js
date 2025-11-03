c9f88f0e46af902d742b66b277d280a742cf933f
B00157434 Kevin Tshiaka <B00157434@mytudublin.ie>
2025-11-03 16:44:58 +0000

    Final commit

--- firestoreConfig.js
+++ firestoreConfig.js
@@ -2,8 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Your Firebase configuration object goes here
-}
+  apiKey: "AIzaSyB...",
+  authDomain: "kevins-project-57434.firebaseapp.com",
+  projectId: "kevins-project-57434",
+  storageBucket: "kevins-project-57434.appspot.com",
+  messagingSenderId: "515197855611",
+  appId: "1:515197855611:web:42a9a7cf48ef9121587fde"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
